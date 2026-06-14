0cdf962622dfc43ad72edf7dd792deae2212d334
Victor Nascimento Bakke <gipphe@gmail.com>
2026-06-14 21:40:39 +0200

    feat: support SPT 4.0.0

--- plugins/basic_games/games/game_spt.py
+++ plugins/basic_games/games/game_spt.py
@@ -1,15 +1,16 @@
 from typing import List, Tuple
-from PyQt6.QtCore import QFileInfo
+
 import mobase
-from ..basic_game import BasicGame
+from PyQt6.QtCore import QFileInfo
+
 from ..basic_features import BasicModDataChecker, GlobPatterns
+from ..basic_game import BasicGame
 
 
 class SPTGame(BasicGame, mobase.IPluginFileMapper):
-
     Name = "SPT Plugin"
     Author = "Archon"
-    Version = "1.1.2"
+    Version = "2.0.0"
     GameName = "SPT"
     GameShortName = "spt"
     GameBinary = "SPT.Launcher.exe"
@@ -23,7 +24,7 @@ class SPTGame(BasicGame, mobase.IPluginFileMapper):
 
     def init(self, organizer: mobase.IOrganizer) -> bool:
         super().init(organizer)
-        if hasattr(self, '_featureMap'):
+        if hasattr(self, "_featureMap"):
             self._featureMap[mobase.ModDataChecker] = SPTModDataChecker()
         else:
             self._register_feature(SPTModDataChecker())
@@ -32,6 +33,20 @@ class SPTGame(BasicGame, mobase.IPluginFileMapper):
     def executables(self) -> List[mobase.ExecutableInfo]:
         execs = super().executables()
 
+        workaroundPath = self.windows_workaround()
+        linux_workaround_path = self.linux_workaround()
+
+        execs.append(
+            mobase.ExecutableInfo("Launch SP Tarkov", QFileInfo(workaroundPath))
+        ).append(
+            mobase.ExecutableInfo(
+                "Launch SPT Tarkov (Linux)", QFileInfo(linux_workaround_path)
+            )
+        )
+        execs.pop(0)
+        return execs
+
+    def windows_workaround(self) -> str:
         """
         A bat script file to bridge the environment to server and launcher.
         """
@@ -47,7 +62,7 @@ class SPTGame(BasicGame, mobase.IPluginFileMapper):
 setlocal
 
 set "launcher_path=SPT.Launcher.exe"
-set "server_path=SPT.Server.exe"
+set "server_path=SPT/SPT.Server.exe"
 
 REM Launch the server.exe
 start "" "%server_path%"
@@ -62,21 +77,41 @@ endlocal
 """
                 )
         workaround.close()
+        return workaroundPath
 
-        execs.append(
-            mobase.ExecutableInfo("Launch SP Tarkov", QFileInfo(workaroundPath))
-        )
-        execs.pop(0)
-        return execs
+    def linux_workaround(self) -> str:
+        workaroundPath = self._gamePath + "/sptvfsbridge.sh"
+
+        try:
+            workaround = open(workaroundPath, "rt")
+        except FileNotFoundError:
+            with open(workaroundPath, "wt") as workaround:
+                workaround.write(
+                    """
+#!/usr/bin/env bash
+launcher_path=SPT.Launcher.exe
+server_path=SPT/SPT.Server.Linux
+nohup x-terminal-emulator -e "bash -c '$server_path'" &
+sleep 5s
+nohup umu-launcher "$launcher_path" &
+                    """
+                )
 
     def mappings(self) -> list[mobase.Mapping]:
         return []
+
 
 class SPTModDataChecker(BasicModDataChecker):
     def __init__(self, patterns: GlobPatterns = GlobPatterns()):
         super().__init__(
             GlobPatterns(
-                valid=["BepInEx", "User"],
-                move={"plugins": "BepInEx/", "patchers": "BepInEx/", "package.json": "User/Mods/", "*.dll": "BepInEx/plugins/", "*": "User/Mods/"},
+                valid=["BepInEx", "SPT"],
+                move={
+                    "plugins": "BepInEx/plugins",
+                    "patchers": "BepInEx/patchers",
+                    "config": "BepInEx/config",
+                    "*": "SPT/user/mods/",
+                },
             ).merge(patterns),
         )
+
